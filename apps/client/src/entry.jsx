9d3b0b5fd1375afbc3c96385565337f3c8dc45ff
martintrifunov <martintrifunov@proton.me>
2025-03-02 15:56:41 +0100

    Fix faulty robot font import

--- apps/client/src/entry.jsx
+++ apps/client/src/entry.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App.jsx";
 import "./assets/index.css";
-import "@fontsource/roboto/400.css";
+import "@fontsource/roboto";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider.jsx";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
